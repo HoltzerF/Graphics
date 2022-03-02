5315b5b0edeaed6520625c60549f6a9cc0f2f3f3
HoltzerF <87450083+HoltzerF@users.noreply.github.com>
2022-03-02 23:20:44 +0100

    Add files via upload

--- rendering/drawPolys.c
+++ rendering/drawPolys.c
@@ -2,8 +2,6 @@
 #include <stdio.h>
 #include <time.h>
 
-// written by Holtzer
-
 // ------------------------------------
 // framebuffer for testing the routines
 // ------------------------------------
@@ -41,11 +39,15 @@ void putPixel(int x, int y, char c){
 // ------------------------------------
 
 typedef struct{
-	float x, y;
+	float x, y, z;
+	float u, v;
 }vert_t;
 
 typedef	struct{
 	float x, sx;
+	float z, sz;
+	float u, su;
+	float v, sv;
 	unsigned int height;
 }edge_t;
 
@@ -63,31 +65,16 @@ void constructEdge(edge_t *e, vert_t* top, vert_t* bot, unsigned int *y_top){
 	
 	float inverseHeight = 1.0 / (bot->y - top->y);
 	e->sx = (bot->x - top->x) * inverseHeight;
+	e->sz = (bot->z - top->z) * inverseHeight;
+	e->su = (bot->u - top->u) * inverseHeight;
+	e->sv = (bot->v - top->v) * inverseHeight;
 	
 	float pre = (float)(*y_top) + 0.5 - top->y;
 	e->x = top->x + e->sx * pre;
+	e->z = top->z + e->sz * pre;
+	e->u = top->u + e->su * pre;
+	e->v = top->v + e->sv * pre;
 }
-
-void drawEdges(edge_t *le, edge_t *re, unsigned int *y){
-	unsigned int height = le->height < re->height ? le->height : re->height;
-	le->height -= height;
-	re->height -= height;
-		
-	while(height--){
-		unsigned int beg = getRaster(le->x);
-		unsigned int end = getRaster(re->x);
-			
-		for(; beg < end; ++beg){
-			putPixel(beg, *y, 'O');
-		}
-			
-		// edge stepping
-		le->x += le->sx;
-		re->x += re->sx;
-		++(*y);
-	}
-}
-
 
 // triangle rasterizer
 void drawTriangle(vert_t *v0, vert_t *v1, vert_t *v2){
@@ -115,6 +102,7 @@ void drawTriangle(vert_t *v0, vert_t *v1, vert_t *v2){
 	iy[1] = getRaster(v1->y);
 	iy[2] = getRaster(v2->y);
 	
+	// calculating edges
 	edge_t topBot, topMid, midBot;
 	constructEdge(&topBot, v0, v2, &iy[0]);
 	constructEdge(&topMid, v0, v1, &iy[0]);
@@ -127,14 +115,63 @@ void drawTriangle(vert_t *v0, vert_t *v1, vert_t *v2){
 	// check if mid is left or right (midleft = 1, means true, the mid is on the left)
 	int midleft = (v1->y - v0->y) * (v2->x - v0->x) > (v1->x - v0->x) * (v2->y - v0->y);
 	
-	// current y
+	// current y and height
 	unsigned int y = iy[0];
-	if(midleft){
-		drawEdges(&topMid, &topBot, &y);
-		drawEdges(&midBot, &topBot, &y);
-	}else{
-		drawEdges(&topBot, &topMid, &y);
-		drawEdges(&topBot, &midBot, &y);
+	unsigned int height;
+	
+	// e[0] = left, e[1] = right
+	edge_t *e[2];
+	e[midleft] = &topBot;
+	e[!midleft] = &topMid;
+	
+	// height from top to bottom
+	height = iy[2] - iy[0];
+	if(height == 0){
+		height = iy[1] - iy[0];
+	}
+	
+	// drawing spans
+	while(height--){
+		
+		if(y == iy[1]){
+			e[!midleft] = &midBot;
+		}
+		
+		float inverseWidth = 1.0 / (e[1]->x - e[0]->x);
+		float sz = (e[1]->z - e[0]->z) * inverseWidth;
+		float su = (e[1]->u - e[0]->z) * inverseWidth;
+		float sv = (e[1]->v - e[0]->v) * inverseWidth;
+		
+		// calculating start and end
+		unsigned int beg = getRaster(e[0]->x);
+		unsigned int end = getRaster(e[1]->x);
+		
+		float pre = (float)beg + 0.5 - e[0]->x;
+		float z = e[0]->z + sz * pre;
+		float u = e[0]->u + sz * pre;
+		float v = e[0]->v + sv * pre;
+		
+		// drawing the span
+		for(; beg < end; ++beg){
+			
+			// pixel plotting
+			putPixel(beg, y, 'O');
+			
+			z += sz;
+			u += su;
+			v += sv;
+		}
+			
+		// edge stepping
+		e[0]->x += e[0]->sx;
+		e[1]->x += e[1]->sx;
+		e[0]->z += e[0]->sz;
+		e[1]->z += e[1]->sz;
+		e[0]->u += e[0]->su;
+		e[1]->u += e[1]->su;
+		e[0]->v += e[0]->sv;
+		e[1]->v += e[1]->sv;
+		++y;
 	}
 }
 
@@ -142,7 +179,7 @@ void drawTriangle(vert_t *v0, vert_t *v1, vert_t *v2){
 void drawPolygon(vert_t *v, unsigned int n){
 	
 	// everything with less than 3 vertices has no area
-	if(n < 2){
+	if(n < 3){
 		return;
 	}
 	if(n == 3){
@@ -150,9 +187,9 @@ void drawPolygon(vert_t *v, unsigned int n){
 	}
 	
 	// storing edge vertex informations
-	int l_top = 0, l_bot, r_top, r_bot;			// indexing vertices by integers
+	int l_top = 0, l_bot, r_top, r_bot;						// indexing vertices by integers
 	unsigned int l_top_iy, l_bot_iy, r_top_iy, r_bot_iy;	// left and right edge integer y's
-	unsigned int i;						// iterator, later the y position
+	unsigned int i;											// iterator, later the y position
 	
 	// getting the top most vertex (lowest y)
 	for(i = 1; i < n; ++i){
@@ -219,8 +256,45 @@ void drawPolygon(vert_t *v, unsigned int n){
 		}
 		
 		// drawing spans between edges
-		drawEdges(&le, &re, &i);
+		unsigned int height = le.height < re.height ? le.height : re.height;
+		le.height -= height;
+		re.height -= height;
 		
+		while(height--){
+		
+			float inverseWidth = 1.0 / (re.x - le.x);
+			float sz = (re.z - le.z) * inverseWidth;
+			float su = (re.u - le.u) * inverseWidth;
+			float sv = (re.v - le.v) * inverseWidth;
+		
+			unsigned int beg = getRaster(le.x);
+			unsigned int end = getRaster(re.x);
+		
+			float pre = (float)beg + 0.5 - le.x;
+			float z = le.z + sz * pre;
+			float u = le.u + su * pre;
+			float v = le.v + sv * pre;
+			
+			for(; beg < end; ++beg){
+				
+				putPixel(beg, i, 'O');
+			
+				z += sz;
+				u += su;
+				v += sv;
+			}
+			
+			// edge stepping
+			le.x += le.sx;
+			re.x += re.sx;
+			le.z += le.sz;
+			re.z += re.sz;
+			le.u += le.su;
+			re.u += re.su;
+			le.v += le.sv;
+			re.v += le.sv;
+			++i;
+		}
 	}while(l_bot != r_bot);
 }
 
@@ -230,7 +304,7 @@ void drawPolygon(vert_t *v, unsigned int n){
 
 // drawing 30 000 polygons
 
-int main(){
+int	main(){
 	
 	clear(' ');
 	int i, frames;
@@ -243,24 +317,22 @@ int main(){
 		// how many polygons
 		for(i = 0; i < 10000; ++i){
 			
-			
-			vert_t v[4];
-			v[0].x = 6;		v[0].y = 7;
-			v[1].x = 11;	v[1].y = 7;
-			v[2].x = 11;	v[2].y = 10;
-			v[3].x = 6;		v[3].y = 12;
-			drawPolygon(v, 4);/**/
-			
-			
-			v[0].x = 2;	v[0].y = 0;
-			v[1].x = 4;	v[1].y = 3;
-			v[2].x = 1;	v[2].y = 3;
+			vert_t v[3];
+			v[0].x = 3;	v[0].y = 5;
+			v[1].x = 5;	v[1].y = 10;
+			v[2].x = 1;	v[2].y = 8;
+			drawPolygon(v, 3);/**/
+	
+	
+			v[0].x = 1.5;	v[0].y = 1.5;
+			v[1].x = 4.5;	v[1].y = 4.5;
+			v[2].x = 1.5;	v[2].y = 4.5;
 			drawPolygon(v, 3);/**/
 			
 			
-			v[0].x = 9;		v[0].y = 0;
-			v[1].x = 11;	v[1].y = 5;
-			v[2].x = 7;		v[2].y = 3;
+			v[0].x = 6;		v[0].y = 1;
+			v[1].x = 10;	v[1].y = 1;
+			v[2].x = 8;		v[2].y = 5;
 			drawPolygon(v, 3);/**/
 			
 		}
